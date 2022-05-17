0a0b6666c0a5ec547d1ec2bef5a1c4d024fb2e85
ossyyrr <ossyyrr@hotmail.com>
2022-05-17 15:37:03 +0200

    vote band

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,36 @@
 const { io } = require('../index.js');
+const Band = require('../models/band.js');
+const Bands = require('../models/bands.js');
+
+const bands = new Bands();
+bands.addBand(new Band( 'Bon Jovi' ));
+bands.addBand(new Band( 'Queen' ));
+bands.addBand(new Band( 'El canto del loco' ));
+bands.addBand(new Band( 'Heroes del silencio' ));
+bands.addBand(new Band( 'ROSALIA' ));
+
+console.log(bands);
+
 
 // Mensajes de sockets
 // Client es el cliente que consulta al servidor. io es la llegada y retorno de salida del servidor
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    // Al conectarse un cliente al servidor.
+    client.emit('active-bands', bands.getBands());
+
+
+    client.on('vote-band', (payload)=>{
+        console.log('vote band');
+        console.log(payload);
+
+        bands.voteBand( payload.id);
+
+        io.emit('active-bands', bands.getBands()); 
+
+    })
+
 
     client.on('disconnect', () => { 
         console.log('Cliente desconectado');
