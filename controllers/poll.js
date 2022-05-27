2407ec3b4e42716e0c84cd6e02f63df71c07c05e
ossyyrr <ossyyrr@hotmail.com>
2022-05-27 11:12:00 +0200

    get polls

--- controllers/poll.js
+++ controllers/poll.js
@@ -1,49 +1,17 @@
 const { response } = require('express');
-const User = require('../models/userSchema');
+const Poll = require('../models/pollSchema');
 const bcrypt = require('bcryptjs');
 
 const getPollsFromDB = async(req, res = response) => {
     console.log('GET POLLs ******');
     console.log(req.body);
+
+    const polls = await Poll.find({  })
+
     res.json({
         ok: true,
-        msg:'getPolls'
+        polls: polls,
       });
-
-
-    // const { email,password } = req.body;
-    // try {
-
-    //     const existeEmail = await User.findOne({ email })
-    //     if(existeEmail){
-    //         return res.status(400).json({
-    //             ok: false,
-    //             msg: 'El correo ya está registrado'
-    //         })
-    //     }
-    //     const usuario = new User(req.body);
-       
-    //     // Encriptar contraseña
-    //     const salt = bcrypt.genSaltSync();
-    //     usuario.password = bcrypt.hashSync( password, salt );
-
-    //     // Guardar usuario en DB
-    //     await usuario.save();
-
-    //     // Generar JWT
-    //     const token = await generarJWT(usuario.uid);
-    //     res.json({
-    //         ok: true,
-    //         usuario,
-    //         token,
-    //     });
-    // } catch (error){
-    //     console.log(error);
-    //     res.status(500).json({
-    //         ok: false,
-    //         msg: 'Error al crear usuario'
-    //     })
-    // }
 }
 
 module.exports={
